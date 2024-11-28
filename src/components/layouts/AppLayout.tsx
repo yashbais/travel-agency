0e4a5ffa3368aec35e226900b44e8403eea7ab50
yashbais <yash.karoscan@gmail.com>
2024-11-28 13:23:25 +0530

    serialize added

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export interface AppLayoutProps {
-    children: React.ReactNode;
-  }
+  children: React.ReactNode;
+}
 
-const AppLayout: React.FC<AppLayoutProps>  = ({children}) => {
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   return (
     <div className='xl:mx-[9.25rem] mx-[3rem] lg:my-2  my-10 absolute z-0 top-[6.3rem] '>{children}</div>
   )
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,24 @@
+import clsx, { type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...args: ClassValue[]) => twMerge(clsx(args));
+
 export const stringReduce = (string: string, length: number) => {
-    return string?.length > length ? string?.substring(0, length) + "..." : string
+  return string?.length > length
+    ? string?.substring(0, length) + "..."
+    : string;
+};
+
+export const serialize = <T extends Record<string, unknown>>(object: T): string => {
+  const str: string[] = [];
+  for (const p in object) {
+    if (Object.prototype.hasOwnProperty.call(object, p)) {
+      const value = object[p]; 
+      if (value !== undefined && (value || typeof value === "boolean" || value === null)) {
+        str.push(`${encodeURIComponent(p)}=${encodeURIComponent(String(value))}`);
+      }
+    }
   }
+
+  return str.join("&");
+};
